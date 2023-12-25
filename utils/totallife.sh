2db4fe1f151a6a1aaef0f3d03a2641833cd696cf
slowrunner <slowrunner@users.noreply.github.com>
2023-12-24 20:08:33 -0500

    added safety shutdown and playtime counts

--- utils/totallife.sh
+++ utils/totallife.sh
@@ -9,11 +9,12 @@ echo "(Cleaning life.log first)"
 echo " "
 fn="/home/pi/wali_pi5/logs/life.log"
 totalLife=`(awk -F':' '{sum+=$3}END{print sum;}' $fn)`
-echo "*** Pi5DESK TOTAL LIFE STATISTICS ***"
-echo "Total Life: " $totalLife "hrs (since Nov 28, 2023)"
-echo "Sessions (boot): " `(grep -c "\- boot \-" $fn)`
+echo "*** Create3-WALI TOTAL LIFE STATISTICS ***"
+echo "Total Life: " $totalLife "hrs (since Dec 11, 2023)"
+echo "Playtimes (Undocked-Docked):" `(grep -c " Docking: success " $fn)`
 booted=`(grep -c "\- boot \-" $fn)`
+echo "Sessions (boot): " `(grep -c "\- boot \-" $fn)`
 aveSession=`(echo "scale=1; ($totalLife / $booted)" | bc -l)`
 echo "Average Session: " $aveSession "hrs"
-safetyShutdowns=`(grep -c "SAFETY SHUTDOWN" $fn)`
+safetyShutdowns=`(grep -c "safety shutdown" $fn)`
 echo "Safety Shutdowns: " $safetyShutdowns 
