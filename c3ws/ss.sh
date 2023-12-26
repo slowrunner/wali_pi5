eba04c1ac3b9dabbfc9e1439e0b164153fe44806
slowrunner <slowrunner@users.noreply.github.com>
2023-12-26 16:41:25 +0000

    fixed to work in docker and before

--- c3ws/ss.sh
+++ c3ws/ss.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-set -e
 
-if [ $ROS_DISTRO == "galactic" ]; then
+if [ -f /opt/ros/galactic/setup.bash ]; then
     source /opt/ros/galactic/setup.bash
-  else
+fi
+
+if [ -f /opt/ros/humble/setup.bash ]; then
     source /opt/ros/humble/setup.bash
 fi
 
-source ~/wali_pi5/c3ws/install/setup.bash
-
+if [ -f /opt/ros/humble/local_setup.bash ]; then
+  if [ -f ~/wali_pi5/c3ws/install/setup.bash ]; then
+   source ~/wali_pi5/c3ws/install/setup.bash
+  fi
+fi
--- configs/append_to_tilda_dot_bashrc.sh
+++ configs/append_to_tilda_dot_bashrc.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 
-echo -e '\n*** Appending: export ROS_LOG_DIR="/home/pi/wali_pi5/c3ws/roslogs" to ~/.bashrc' 
-echo 'export ROS_LOG_DIR="/home/pi/wali_pi5/c3ws/roslogs"' >> ~/.bashrc
+echo -e '\n*** Appending: export ROS_LOG_DIR="/home/pi/wali_pi5/c3ws/roslogs"' to ~/.bashrc
+echo -e '\nexport ROS_LOG_DIR="/home/pi/wali_pi5/c3ws/roslogs"' >> ~/.bashrc
+
+echo -e '\n*** Appending: export ROS_DISTRO="humble"' to ~/.bashrc
+echo 'export ROS_DISTRO="humble"' >> ~/.bashrc
+
+echo -e '\n** Appending source ss.sh if exists to ~/.bashrc'
+echo -e '\n' >> ~/.bashrc
+echo "if [ -f /home/pi/wali_pi5/c3ws/ss.sh ]; then " >> ~/.bashrc
+echo "    source /home/pi/wali_pi5/c3ws/ss.sh " >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+
+echo -e '\n# If ssh closes immediately, use "ssh -t pi@x.x.x.x /bin/sh" and fix ~/.bashrc' >> ~/.bashrc
+
+
+
 echo -e '\n*** tail ~/.bashrc:'
 tail ~/.bashrc
 echo -e '\n'
