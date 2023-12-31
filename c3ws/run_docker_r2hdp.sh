35eea8345c267a4e2cd83fb9de4597042525121b
slowrunner <slowrunner@users.noreply.github.com>
2023-12-31 09:44:55 -0500

    doc difference from start_docker_service

--- c3ws/run_docker_r2hdp.sh
+++ c3ws/run_docker_r2hdp.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# FILE: run_docker_r2hdp.sh
+
+# ONLY STARTS ROS 2 HUMBLE DOCKER CONTAINER (NO WALI NODES)
+
 cd ~/wali_pi5/c3ws
 # --rm removes container after exit
 # --w working dir to start in
-echo -e "\n*** STARTING ROS 2 HUMBLE PLUS IN DOCKER ***"
+echo -e "\n*** STARTING ONLY ROS 2 HUMBLE PLUS IN DOCKER (NO WALI NODES) ***"
 docker run -it --net=host  -v /home/pi:/home/pi -v /dev/input:/dev/input \
  -v /dev/bus/usb:/dev/bus/usb  -w /home/pi/wali_pi5/c3ws --privileged --rm r2hdp
 echo -e "\n*** EXITED DOCKER ***"
