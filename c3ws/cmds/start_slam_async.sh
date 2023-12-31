097d3cb04debc684730557a1b784173c81d42ce4
slowrunner <slowrunner@users.noreply.github.com>
2023-12-31 13:50:44 -0500

    cleaning

--- c3ws/cmds/start_slam_async.sh
+++ c3ws/cmds/start_slam_async.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-echo -e "\n*** STARTING ROS2 SLAM-TOOLBOX "
+echo -e "\n*** STARTING ROS2 SLAM-TOOLBOX with params/slam.Create3.yaml"
 echo "*** Drive Wali around room, generating /map topics"
 echo "*** (Async: Process scans as able to, online))"
-echo "*** ros2 launch slam_toolbox online_async_launch.py 'slam_params_file:=./my_mapper_params_online_async.yaml"
-# ros2 launch slam_toolbox online_sync_launch.py 'slam_params_file:=/home/pi/wali_pi5/c3ws/params/my_mapper_params_online_async.yaml'
-ros2 launch slam_toolbox online_sync_launch.py 'slam_params_file:=/home/pi/wali_pi5/c3ws/params/mapper_params_online_async.yaml'
+echo "*** ASSUMES IR2SCAN NODE IS RUNNING"
+echo "*** ros2 launch slam_toolbox online_async_launch.py 'slam_params_file:=/home/pi/wali_pi5/c3ws/params/map.create3.yaml"
+ros2 launch slam_toolbox online_async_launch.py 'slam_params_file:=/home/pi/wali_pi5/c3ws/params/map.create3.yaml'
 
