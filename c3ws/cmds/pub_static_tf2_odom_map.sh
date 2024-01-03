5593fa844abf4baccc68232519224b941889b292
slowrunner <slowrunner@users.noreply.github.com>
2024-01-02 20:46:18 -0500

    add reference to tf2 tutorial

--- c3ws/cmds/pub_static_tf2_odom_map.sh
+++ c3ws/cmds/pub_static_tf2_odom_map.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Publish static transform from odom to map
+# REF: https://docs.ros.org/en/eloquent/Tutorials/tf2.html
 
 echo -e "ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 odom map"
 ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 odom map
