a33559123fe209f94cf4733766386e8fae0a1549
slowrunner <slowrunner@users.noreply.github.com>
2024-01-06 22:27:01 -0500

    fix permissions on /dev/snd for pi user aplay

--- c3ws/cmds/say.sh
+++ c3ws/cmds/say.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo $1 | piper   --model /home/pi/wali_pi5/c3ws/models/piper-tts/en_US-arctic-medium.onnx    --output_raw | aplay -D plughw:2,0 -r 22050
- -f S16_LE -t raw - 
+# echo $1 | piper   --model /home/pi/wali_pi5/c3ws/models/piper-tts/en_US-arctic-medium.onnx    --output_raw | sudo aplay -D plughw:2,0 -r 22050 -f S16_LE -t raw - 
+echo $1 | piper   --model /home/pi/wali_pi5/c3ws/models/piper-tts/en_US-arctic-medium.onnx  --output_raw | aplay -D plughw:2,0 -r 22050 -f S16_LE -t raw - 
--- configs/docker/start_docker_detached_r2hdp.sh
+++ configs/docker/start_docker_detached_r2hdp.sh
@@ -4,7 +4,9 @@ cd /home/pi/wali_pi5/c3ws
 # --rm removes container after exit
 # --w working dir to start in
 echo -e "\n*** STARTING ROS 2 HUMBLE PLUS IN DOCKER ***"
-docker run -dit --name r2hdp --net=host --device /dev/snd \
+# docker run -dit --name r2hdp --net=host --device /dev/snd \
+docker run -dit --name r2hdp --net=host \
+ -v /dev/snd:/dev/snd \
  -v /home/pi:/home/pi -v /dev/input:/dev/input \
  -v /dev/bus/usb:/dev/bus/usb  \
  -e TZ=America/New_York \
