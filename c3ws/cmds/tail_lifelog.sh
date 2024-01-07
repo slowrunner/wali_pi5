404a86a27c56370419572bcd2f3a7d312e92dea4
slowrunner <slowrunner@users.noreply.github.com>
2024-01-07 15:42:35 -0500

    more entries

--- c3ws/cmds/tail_lifelog.sh
+++ c3ws/cmds/tail_lifelog.sh
@@ -2,4 +2,4 @@
 echo -e "\n*** CLEAN LIFE LOG ***"
 /home/pi/wali_pi5/plib/cleanlifelog.py
 echo -e "\n*** TAIL LIFE LOG ***"
-tail -10 /home/pi/wali_pi5/logs/life.log
+tail -30 /home/pi/wali_pi5/logs/life.log
