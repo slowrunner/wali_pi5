b9aae2b974446077df77fdc7ab15896f1185a0bd
slowrunner <slowrunner@users.noreply.github.com>
2024-02-19 11:02:21 -0500

    standalone start

---
+++ c3ws/cmds/start_discovery_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+export ROS_DISCOVERY_SERVER="127.0.0.1:11811"
+export FASTRTPS_DEFAULT_PROFILES_FILE=/home/pi/wali_pi5/configs/super_client_configuration_file.xml
+
+# echo -e "\n*** Start FastDDS Discovery Server"
+echo '*** fastdds discovery -i 0 -l 192.168.186.3 -p 11811 &'
+fastdds discovery -i 0 -l 192.168.186.3 -p 11811 -l 10.0.0.219 -p 11811 -l 127.0.0.1 -p 11811 &
+
